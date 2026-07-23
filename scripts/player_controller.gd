944ec277397e62fc09e292f94bd359a50ee9c29a
DavidAdeboye <davidadeboye47@gmail.com>
2026-07-24 00:46:59 +0100

    feat: implement first-person player controller and base interactable system with a test environment

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 ## Attach this script to a CharacterBody3D root that has:
 ##   - CollisionShape3D (capsule, matching a human-ish height)
 ##   - A Node3D called "Head" as a child, with a Camera3D as its child
+##   - Camera3D should have a RayCast3D child pointed forward (Z: -3)
 ## Head handles vertical look (pitch); the body handles horizontal look (yaw).
 
 @export var walk_speed: float = 3.5
@@ -12,8 +13,10 @@ extends CharacterBody3D
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
+@onready var interact_ray: RayCast3D = $Head/Camera3D/RayCast3D
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var current_interactable: Interactable = null
 
 
 func _ready() -> void:
@@ -30,6 +33,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		head.rotate_x(-event.relative.y * mouse_sensitivity)
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+
+	if event.is_action_pressed("interact") and current_interactable:
+		current_interactable.interact()
 
 
 func _physics_process(delta: float) -> void:
@@ -50,3 +56,13 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, walk_speed)
 
 	move_and_slide()
+	_update_interactable()
+
+
+func _update_interactable() -> void:
+	if interact_ray.is_colliding():
+		var collider = interact_ray.get_collider()
+		if collider is Interactable:
+			current_interactable = collider
+			return
+	current_interactable = null
